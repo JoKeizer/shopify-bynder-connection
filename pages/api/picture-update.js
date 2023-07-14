462d596198c8273ed8b001cc827a99581e80bb7b
Jolanda Keizer <keizer2012@gmail.com>
2023-07-14 12:07:39 +0200

    added token

--- pages/api/picture-update.js
+++ pages/api/picture-update.js
@@ -15,15 +15,6 @@ const bynder = new Bynder({
 });
 
 
-const response = await fetch('https://httpbin.org/post', {
-	method: 'post',
-	body: JSON.stringify(body),
-	headers: {'Content-Type': 'application/json'}
-});
-const data = await response.json();
-
-console.log(data);
-
 //this is the asyncronous function that takes care of posting the pictures to bynder  (it gets called on line 93)
 async function pushImagesToShopify(images, productId) {
   console.log("push images to shoify")
